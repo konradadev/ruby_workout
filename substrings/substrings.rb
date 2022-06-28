45db23e2c00fb1313e90a616ca3a2ca924ce57d0
konradadev <konradadev@gmail.com>
2022-06-28 15:17:36 +0200

    Add a string for testing

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,6 +1,8 @@
 dictionary1 = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-dictionary2 = ["above","up","stop","standing","bald","now","heloo","is","you","high","lend","complete","enemy","stand"]
+dictionary2 = ["above","up","stop","standing","bald","now","hello","is","you","high","lend","complete","enemy","stand"]
+
+string = "Howdy partner, sit down! How's it going? Is your enemy still around? Will you stand up and above anyone else to stop him?"
 
 def substrings(string, dictionary)
-  
+
 end
