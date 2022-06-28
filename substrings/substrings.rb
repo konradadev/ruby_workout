fab99210f90490fffdc9ecc56e5d30ab2db3cea3
konradadev <konradadev@gmail.com>
2022-06-28 15:07:25 +0200

    Add alternate dictionary

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,1 +1,6 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary1 = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary2 = ["above","up","stop","standing","bald","now","heloo","is","you","high","lend","complete","enemy","stand"]
+
+def substrings(string, dictionary)
+  
+end
