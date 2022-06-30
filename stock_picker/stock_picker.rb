9246245bd91d2800763438f7c43fc6ca38bfd091
konradadev <konradadev@gmail.com>
2022-06-30 16:23:05 +0200

    Add a function to check if it's possible to buy and sell

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,5 +1,13 @@
+def check_timeline(lowests_index, highests_index)
+  if lowests_index < highests_index
+    return [lowests_index, lowests_index]
+  else
+    return nil
+  end
+end
+
 def stock_picker(stock_prices)
-  
+
 end
 
 stock_prices_example = [17,3,6,9,15,8,6,1,10]
