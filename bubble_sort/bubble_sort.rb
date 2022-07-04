8875d3fe3fedeac84ab246629b5066c6dcb079aa
konradadev <konradadev@gmail.com>
2022-07-04 12:18:23 +0200

    Made bubble_sort() functional

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,5 +1,19 @@
 def bubble_sort(unsorted_array)
+  for i in 1..unsorted_array.length
+    unsorted_array.each_with_index do |number, index|
+      unless index == unsorted_array.length-1
+        next_number = unsorted_array[index+1]
+        if next_number < number
+          helper = number
+          unsorted_array[index] = next_number
+          unsorted_array[index+1] = helper
+        end
+      end
+    end
+  end
+  return unsorted_array
 end
 
 array_example = [4,3,78,2,0,2]
-bubble_sort(array_example)
+array_sorted = bubble_sort(array_example)
+print array_sorted
